29e6a954bc30bd517a12bf50ef0ad308bba95d9b
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 12:36:39 -0300

    Update Funcionario

--- funcionario/src/main/java/br/com/funcionario/domain/Funcionario.java
+++ funcionario/src/main/java/br/com/funcionario/domain/Funcionario.java
@@ -1,6 +1,7 @@
 package br.com.funcionario.domain;
 
 import br.com.funcionario.domain.enuns.EstadoFuncionarioEnum;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,14 +21,13 @@ public class Funcionario {
 
     @Column(name = "numero_funcionario", length = 20)
     private Integer numeroFuncionario;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "estado_funcionario", length = 12)
     private EstadoFuncionarioEnum estadoFuncionarioEnum;
-
+    @Getter(onMethod = @__({@JsonIgnore}))
     @OneToOne(mappedBy = "funcionario", cascade = CascadeType.PERSIST)
     private FuncionarioClt funcionarioClt;
-
+    @Getter(onMethod = @__({@JsonIgnore}))
     @OneToOne(mappedBy = "funcionario", cascade = CascadeType.PERSIST)
     private FuncionarioCnpj funcionarioCnpj;
 }
