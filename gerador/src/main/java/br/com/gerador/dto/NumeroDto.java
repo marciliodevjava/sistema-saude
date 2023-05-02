06ec7e34cb73745e8310f7d625ddad5ecc330da0
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 17:17:50 -0300

    Create NumeroDto

---
+++ gerador/src/main/java/br/com/gerador/dto/NumeroDto.java
@@ -0,0 +1,18 @@
+package br.com.gerador.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class NumeroDto {
+
+    private Integer numero;
+    private LocalDateTime localDateTime;
+}
