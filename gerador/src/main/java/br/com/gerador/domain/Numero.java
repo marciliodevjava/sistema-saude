22d8227f365f0ec46e462e11d01843083aa9a947
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 17:17:32 -0300

    Create numero

---
+++ gerador/src/main/java/br/com/gerador/domain/Numero.java
@@ -0,0 +1,23 @@
+package br.com.gerador.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "numeros")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Numero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer numero;
+    private LocalDateTime data;
+}
