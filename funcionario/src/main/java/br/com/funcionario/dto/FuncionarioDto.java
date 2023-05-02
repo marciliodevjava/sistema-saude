aa3f7be8eded1d4b087f269fd5b3a85e7c92c8f1
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 12:13:25 -0300

    Create FuncionarioDto.

---
+++ funcionario/src/main/java/br/com/funcionario/dto/FuncionarioDto.java
@@ -0,0 +1,16 @@
+package br.com.funcionario.dto;
+
+import br.com.funcionario.domain.FuncionarioClt;
+import br.com.funcionario.domain.FuncionarioCnpj;
+import br.com.funcionario.domain.enuns.EstadoFuncionarioEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FuncionarioDto {
+
+    private EstadoFuncionarioEnum estadoFuncionarioEnum;
+    private FuncionarioClt funcionarioClt;
+    private FuncionarioCnpj funcionarioCnpj;
+}
