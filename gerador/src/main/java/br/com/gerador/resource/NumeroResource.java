f099962ac3928f47773c87e3575a221887e74924
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 17:19:10 -0300

    Create NumeroResource

---
+++ gerador/src/main/java/br/com/gerador/resource/NumeroResource.java
@@ -0,0 +1,30 @@
+package br.com.gerador.resource;
+
+import br.com.gerador.dto.NumeroDto;
+import br.com.gerador.service.NumeroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gerador-numero")
+public class NumeroResource {
+
+    @Autowired
+    private NumeroService numeroService;
+
+    @GetMapping()
+    public ResponseEntity<NumeroDto> gerarNumero(){
+        NumeroDto numeroRetorno = numeroService.geraNumero();
+
+        return ResponseEntity.ok(numeroRetorno);
+    }
+
+    @GetMapping("/port")
+    public String retornaPort(@Value("${local.server.port}") String port) {
+        return String.format("Requisição respondida pela instancia na porta %s", port);
+    }
+}
