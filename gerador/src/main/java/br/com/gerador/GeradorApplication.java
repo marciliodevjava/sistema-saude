0ced8f7fc91c10bbedd0a37aed931f98813753f0
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 12:41:16 -0300

    Gerador de Número

---
+++ gerador/src/main/java/br/com/gerador/GeradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorApplication.class, args);
+	}
+
+}
