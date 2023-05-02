1fe9a267a3e0724cdefa863900ea716f2f4a32ee
marciliodevjava <marciliodevjava@gmail.com>
2023-05-02 17:19:32 -0300

    Create NumeroService

---
+++ gerador/src/main/java/br/com/gerador/service/NumeroService.java
@@ -0,0 +1,40 @@
+package br.com.gerador.service;
+
+import br.com.gerador.domain.Numero;
+import br.com.gerador.dto.NumeroDto;
+import br.com.gerador.repository.NumeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class NumeroService {
+
+    @Autowired
+    private NumeroRepository numeroRepository;
+
+    public NumeroDto geraNumero(){
+        Numero consulta = numeroRepository.findTopByOrderByIdDesc();
+
+        Integer numero = consulta.getNumero();
+        Numero inserir = this.montarDados(numero);
+
+        this.numeroRepository.save(inserir);
+
+        return new NumeroDto(inserir.getNumero(), inserir.getData());
+    }
+
+    private Numero montarDados(Integer numero) {
+        Numero numeroInserir = new Numero();
+        LocalDateTime dateTime = LocalDateTime.now();
+        Integer valor = 1;
+        Integer soma = numero + valor;
+
+        numeroInserir.setNumero(soma);
+        numeroInserir.setData(dateTime);
+
+        return numeroInserir;
+    }
+}
