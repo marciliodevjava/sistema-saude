1168f78dea3d3c0624c091cee4b4720e57089493
marciliodevjava <marciliodevjava@gmail.com>
2023-05-03 12:35:49 -0300

    Subindo o projeto de Governancia

---
+++ gorvenancia/src/main/java/br/com/gorvenancia/GorvenanciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gorvenancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorvenanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorvenanciaApplication.class, args);
+	}
+
+}
---
+++ gorvenancia/src/test/java/br/com/gorvenancia/GorvenanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gorvenancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorvenanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
