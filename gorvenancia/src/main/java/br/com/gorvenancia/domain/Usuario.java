c32d1c5d9a470bd64cb1f11a49a40ad887e78828
marciliodevjava <marciliodevjava@gmail.com>
2023-05-08 14:01:18 -0300

    Create Usuario

---
+++ gorvenancia/src/main/java/br/com/gorvenancia/domain/Usuario.java
@@ -0,0 +1,21 @@
+package br.com.gorvenancia.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
