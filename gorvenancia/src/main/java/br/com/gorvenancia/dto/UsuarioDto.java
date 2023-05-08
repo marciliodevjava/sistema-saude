ba654ab15efcc83b12775efe12612a7fcb16cfec
marciliodevjava <marciliodevjava@gmail.com>
2023-05-08 14:02:10 -0300

    Create UsuarioDto

---
+++ gorvenancia/src/main/java/br/com/gorvenancia/dto/UsuarioDto.java
@@ -0,0 +1,11 @@
+package br.com.gorvenancia.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioDto {
+    private String login;
+    private String senha;
+}
