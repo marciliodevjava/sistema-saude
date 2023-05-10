79cccdd1c56b77cd7627efb06c1ed69df2861e3d
marciliodevjava <marciliodevjava@gmail.com>
2023-05-09 22:30:54 -0300

    Update HashUtil

--- gorvenancia/src/main/java/br/com/gorvenancia/util/HashUtil.java
+++ gorvenancia/src/main/java/br/com/gorvenancia/util/HashUtil.java
@@ -4,7 +4,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class HashUtil {
 
-    public static String getSecureHash(String text){
+    public static String getSecureHash(String text) {
         String hash = DigestUtils.sha256Hex(text);
         return hash;
     }
