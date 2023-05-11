2775f14fd76c699803547be26ead62d97aac6a14
marciliodevjava <marciliodevjava@gmail.com>
2023-05-11 20:27:05 -0300

    Create FuncionarioCnpjResource.

---
+++ funcionario/src/main/java/br/com/funcionario/resource/FuncionarioCnpjResource.java
@@ -0,0 +1,14 @@
+package br.com.funcionario.resource;
+
+import br.com.funcionario.service.FuncionarioCnpjService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/funcionario/cnpj")
+public class FuncionarioCnpjResource {
+
+    @Autowired
+    private FuncionarioCnpjService funcionarioCnpjService;
+}
