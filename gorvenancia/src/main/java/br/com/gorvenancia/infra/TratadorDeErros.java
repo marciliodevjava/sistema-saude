868e773f444619b4adda86e9934b6eee6166f7c7
marciliodevjava <marciliodevjava@gmail.com>
2023-05-12 10:55:12 -0300

    Create TratadorDeErros

---
+++ gorvenancia/src/main/java/br/com/gorvenancia/infra/TratadorDeErros.java
@@ -0,0 +1,15 @@
+package br.com.gorvenancia.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+}
