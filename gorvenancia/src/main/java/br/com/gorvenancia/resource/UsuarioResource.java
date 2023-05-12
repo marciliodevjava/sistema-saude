d4b6e8cc5e4ac31686aff2eda87d793c16056399
marciliodevjava <marciliodevjava@gmail.com>
2023-05-12 10:59:38 -0300

    Update UsuarioResource

--- gorvenancia/src/main/java/br/com/gorvenancia/resource/UsuarioResource.java
+++ gorvenancia/src/main/java/br/com/gorvenancia/resource/UsuarioResource.java
@@ -1,11 +1,16 @@
 package br.com.gorvenancia.resource;
 
+import br.com.gorvenancia.dto.DadosAutenticacaoDto;
 import br.com.gorvenancia.dto.UsuarioDto;
 import br.com.gorvenancia.service.UsuariosService;
+import br.com.gorvenancia.util.HashUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,10 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioResource {
 
     @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
     private UsuariosService usuariosService;
 
     @PostMapping
-    public ResponseEntity<UsuarioDto> cadastrar(@RequestBody UsuarioDto usuario) {
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDto dados){
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<UsuarioDto> cadastrar(@RequestBody @Valid UsuarioDto usuario) {
 
         UsuarioDto retorno = usuariosService.createUsuario(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(retorno);
