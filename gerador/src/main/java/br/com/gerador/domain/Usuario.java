0892aa6812d59323b526b22bea2c9cf391a4d994
marciliodevjava <marciliodevjava@gmail.com>
2023-05-15 17:29:20 -0300

    Create Usuario

---
+++ gerador/src/main/java/br/com/gerador/domain/Usuario.java
@@ -0,0 +1,22 @@
+package br.com.gerador.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
