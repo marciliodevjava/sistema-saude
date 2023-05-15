b3a9cd5e61799f418acfda0a72955bd54f5b1e90
marciliodevjava <marciliodevjava@gmail.com>
2023-05-15 17:29:35 -0300

    Create UsuarioRepository

---
+++ gerador/src/main/java/br/com/gerador/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.gerador.repository;
+
+import br.com.gerador.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
