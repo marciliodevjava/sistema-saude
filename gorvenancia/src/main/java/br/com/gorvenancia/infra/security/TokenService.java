e9931e515befa39c917b534ebbb72f2c2614df82
marciliodevjava <marciliodevjava@gmail.com>
2023-05-15 13:10:40 -0300

    Update TokenService

--- gorvenancia/src/main/java/br/com/gorvenancia/infra/security/TokenService.java
+++ gorvenancia/src/main/java/br/com/gorvenancia/infra/security/TokenService.java
@@ -2,8 +2,10 @@ package br.com.gorvenancia.infra.security;
 
 import br.com.gorvenancia.domain.Usuario;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +33,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token JWT ", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+           return JWT.require(algorithm)
+                    .withIssuer("API Governancia")
+                    .build()
+                   .verify(tokenJWT)
+                   .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado.");
         }
     }
 
