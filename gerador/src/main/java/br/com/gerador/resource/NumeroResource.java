326b3ca2b3af70899dfa31ba085500db5c68e522
marciliodevjava <marciliodevjava@gmail.com>
2023-05-16 12:49:21 -0300

    Update NumeroResource

--- gerador/src/main/java/br/com/gerador/resource/NumeroResource.java
+++ gerador/src/main/java/br/com/gerador/resource/NumeroResource.java
@@ -4,8 +4,6 @@ import br.com.gerador.dto.NumeroDto;
 import br.com.gerador.service.NumeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
