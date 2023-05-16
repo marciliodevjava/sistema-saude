c506245e82d32e9bbd84d47cdc3c42b16851fefe
marciliodevjava <marciliodevjava@gmail.com>
2023-05-16 19:55:14 -0300

    Update DadosTokenJWT

--- gorvenancia/src/main/java/br/com/gorvenancia/dto/DadosTokenJWT.java
+++ gorvenancia/src/main/java/br/com/gorvenancia/dto/DadosTokenJWT.java
@@ -3,5 +3,5 @@ package br.com.gorvenancia.dto;
 import java.time.Instant;
 import java.time.LocalDateTime;
 
-public record DadosTokenJWT(String login, LocalDateTime dataInicial, LocalDateTime dataFinal, String tokenJWT) {
+public record DadosTokenJWT(String login, String tokenJWT, LocalDateTime dataInicial, LocalDateTime dataFinal) {
 }
