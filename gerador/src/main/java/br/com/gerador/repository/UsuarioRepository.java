d62e988c616c00875f2e2ac88f46c94ac903e7f0
marciliodevjava <marciliodevjava@gmail.com>
2023-05-16 18:35:37 -0300

    Update UsuarioRepository

--- gerador/src/main/java/br/com/gerador/repository/UsuarioRepository.java
+++ gerador/src/main/java/br/com/gerador/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package br.com.gerador.repository;
 
 import br.com.gerador.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    UserDetails findByLogin(String username);
 }
