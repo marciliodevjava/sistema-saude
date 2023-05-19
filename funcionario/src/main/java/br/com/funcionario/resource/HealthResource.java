24ffd8e7f90bdda4e1ab30738d28d67cef0344a4
marciliodevjava <marciliodevjava@gmail.com>
2023-05-19 20:51:33 -0300

    Create HealthResource

---
+++ funcionario/src/main/java/br/com/funcionario/resource/HealthResource.java
@@ -0,0 +1,27 @@
+package br.com.funcionario.resource;
+
+import br.com.funcionario.dto.Health;
+import org.apache.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.beans.Transient;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/health")
+public class HealthResource {
+
+    @GetMapping
+    public ResponseEntity<Health> health(){
+        Health dados = this.gerarHealt();
+        return ResponseEntity.ok(dados);
+    }
+
+    @Transient
+    private Health gerarHealt() {
+        return new Health("API_FUNCIONARIO", LocalDateTime.now(), HttpStatus.SC_OK);
+    }
+}
