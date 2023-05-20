43273d0f55886ce8609604625d87f817d1c57dd8
marciliodevjava <marciliodevjava@gmail.com>
2023-05-19 21:05:12 -0300

    Update HealthResource

---
+++ eureka-serve/src/main/java/br/com/eureka/eurekaserve/resource/HealthResource.java
@@ -0,0 +1,27 @@
+package br.com.eureka.eurekaserve.resource;
+
+import br.com.eureka.eurekaserve.dto.Health;
+import org.apache.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.beans.Transient;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/health")
+public class HealthResource {
+
+    @GetMapping
+    public ResponseEntity<Health> health(){
+        Health dados = this.gerarHealt();
+        return ResponseEntity.ok(dados);
+    }
+
+    @Transient
+    private Health gerarHealt() {
+        return new Health("API_Eureka", LocalDateTime.now(), HttpStatus.SC_OK);
+    }
+}
