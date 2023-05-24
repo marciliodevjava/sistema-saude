7fd4dd057854ddd1f0d9c1fae4cac2b7d82328b7
marciliodevjava <marciliodevjava@gmail.com>
2023-05-23 22:51:05 -0300

    Alteração de pasta, para uma subpasta na mesma pasta chamada domain.

--- funcionario/src/main/java/br/com/funcionario/infra/exception/domain/FuncionarioCnpjException.java
+++ funcionario/src/main/java/br/com/funcionario/infra/exception/domain/FuncionarioCnpjException.java
@@ -1,4 +1,4 @@
-package br.com.funcionario.infra.exception;
+package br.com.funcionario.infra.exception.domain;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
