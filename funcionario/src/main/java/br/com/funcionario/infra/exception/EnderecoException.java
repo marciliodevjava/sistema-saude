0248cfc50a00aa655c8d7b04fa55dc3cd585a940
marciliodevjava <marciliodevjava@gmail.com>
2023-05-23 22:47:20 -0300

    Create EnderecoException

---
+++ funcionario/src/main/java/br/com/funcionario/infra/exception/EnderecoException.java
@@ -0,0 +1,20 @@
+package br.com.funcionario.infra.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NO_CONTENT)
+public class EnderecoException extends RuntimeException {
+
+    public EnderecoException() {
+        super();
+    }
+
+    public EnderecoException(String mensagem) {
+        super(mensagem);
+    }
+
+    public EnderecoException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
+}
