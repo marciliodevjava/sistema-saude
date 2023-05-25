71d7ef086759beb02152115c930c749eab41697b
marciliodevjava <marciliodevjava@gmail.com>
2023-05-25 18:25:37 -0300

    Update SecurityFilter

--- gerador/src/main/java/br/com/gerador/infa/security/filter/SecurityFilter.java
+++ gerador/src/main/java/br/com/gerador/infa/security/filter/SecurityFilter.java
@@ -3,6 +3,7 @@ package br.com.gerador.infa.security.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,6 +17,18 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String tokenJWT = recurarToken(request);
+
         filterChain.doFilter(request, response);
+    }
+
+    private String recurarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeça~ho Authorization");
+        }
+
+        return authorizationHeader;
     }
 }
