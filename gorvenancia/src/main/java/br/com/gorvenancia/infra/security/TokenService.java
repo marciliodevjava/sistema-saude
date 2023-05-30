383d1f5e879e6c8d2ce41587cd3fd66ab651b027
marciliodevjava <marciliodevjava@gmail.com>
2023-05-29 21:13:35 -0300

    Update TokenService

--- gorvenancia/src/main/java/br/com/gorvenancia/infra/security/TokenService.java
+++ gorvenancia/src/main/java/br/com/gorvenancia/infra/security/TokenService.java
@@ -2,7 +2,6 @@ package br.com.gorvenancia.infra.security;
 
 import br.com.gorvenancia.domain.Usuario;
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
@@ -10,12 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
-
-import static com.auth0.jwt.algorithms.Algorithm.*;
 
 @Service
 public class TokenService {
