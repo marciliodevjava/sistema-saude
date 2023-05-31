5af71cdfedf9287d58ad4b55fa3fcf009f18c4c1
marciliodevjava <marciliodevjava@gmail.com>
2023-05-31 16:21:21 -0300

    Update FuncionarioResource

--- funcionario/src/main/java/br/com/funcionario/resource/v0/FuncionarioResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/v0/FuncionarioResource.java
@@ -1,15 +1,49 @@
 package br.com.funcionario.resource.v0;
 
+import br.com.funcionario.dto.FuncionarioDto;
+import br.com.funcionario.resource.imp.FuncionarioImp;
 import br.com.funcionario.service.FuncionarioService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/funcionario")
-public class FuncionarioResource {
+public class FuncionarioResource implements FuncionarioImp {
 
     @Autowired
     private FuncionarioService funcionarioService;
 
+    @PostMapping
+    @Override
+    public ResponseEntity<FuncionarioDto> salvaFuncionario(FuncionarioDto funcionarioDto) {
+        return null;
+    }
+
+    @PutMapping
+    @Override
+    public ResponseEntity<FuncionarioDto> atualizarFuncionario(FuncionarioDto funcionarioDto) {
+        return null;
+    }
+
+    @GetMapping
+    @Override
+    public ResponseEntity<List<FuncionarioDto>> listarFuncionario() {
+        return null;
+    }
+
+    @GetMapping("/listar/{id}")
+    @Override
+    public ResponseEntity<FuncionarioDto> listarFuncionarioId(@PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    @Override
+    public ResponseEntity<FuncionarioDto> deletarFuncionario(@PathVariable Long id) {
+        return null;
+    }
 }
