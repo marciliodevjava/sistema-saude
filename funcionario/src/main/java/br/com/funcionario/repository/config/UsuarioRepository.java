d894a5faadb7367466ea4651b2741753b03fb8c6
marciliodevjava <marciliodevjava@gmail.com>
2023-06-01 15:33:17 -0300

    Update UsuarioRepository

--- funcionario/src/main/java/br/com/funcionario/repository/config/UsuarioRepository.java
+++ funcionario/src/main/java/br/com/funcionario/repository/config/UsuarioRepository.java
@@ -3,7 +3,9 @@ package br.com.funcionario.repository.config;
 import br.com.funcionario.domain.config.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     UserDetails findByLogin(String login);
 }
