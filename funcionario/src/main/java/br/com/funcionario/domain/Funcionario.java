2aca43b799c22f18c9308970d3c72df3988b46a8
marciliodevjava <marciliodevjava@gmail.com>
2023-06-01 15:26:17 -0300

    Update Funcionario

--- funcionario/src/main/java/br/com/funcionario/domain/Funcionario.java
+++ funcionario/src/main/java/br/com/funcionario/domain/Funcionario.java
@@ -8,16 +8,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
 @Entity
 @Table(name = "funcionarios")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Funcionario {
+public class Funcionario implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID identificadorFuncionario;
     @Column(name = "numero_funcionario", length = 20)
     private Integer numeroFuncionario;
     @Enumerated(EnumType.STRING)
