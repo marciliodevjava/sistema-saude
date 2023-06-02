7ec01bc0dc6521ede16884a7f188b7b1e20a1db4
marciliodevjava <marciliodevjava@gmail.com>
2023-06-01 22:28:39 -0300

    Create AuxilioAlimentacaoRetornoDto

---
+++ funcionario/src/main/java/br/com/funcionario/dto/response/AuxilioAlimentacaoRetornoDto.java
@@ -0,0 +1,15 @@
+package br.com.funcionario.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class AuxilioAlimentacaoRetornoDto {
+    private UUID identificadorAuxilioTransporte;
+    private int dias;
+    private BigDecimal valor;
+}
