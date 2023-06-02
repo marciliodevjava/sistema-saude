d71d89ba8a3b9e13d88385b6c74ba039f5206b56
marciliodevjava <marciliodevjava@gmail.com>
2023-06-02 16:57:47 -0300

    Update EnderecoDto

--- funcionario/src/main/java/br/com/funcionario/dto/request/EnderecoDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/request/EnderecoDto.java
@@ -1,15 +1,22 @@
 package br.com.funcionario.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EnderecoDto {
+    @NotNull(message = "E-mail do Funcionário obrigatório.")
     private String cep;
+    @NotNull(message = "E-mail do Funcionário obrigatório.")
     private String logradouro;
+    @NotNull(message = "Número do Funcionário obrigatório.")
     private String numero;
+    @NotNull(message = "Bairro do Funcionário obrigatório.")
     private String bairro;
+    @NotNull(message = "Cidade do Funcionário obrigatório.")
     private String cidade;
+    @NotNull(message = "Uf do Funcionário obrigatório.")
     private String uf;
 }
