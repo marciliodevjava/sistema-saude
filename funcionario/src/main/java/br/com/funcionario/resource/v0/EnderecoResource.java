30bfe93d204880f17ce967218e2dc27c1decc05e
marciliodevjava <marciliodevjava@gmail.com>
2023-06-02 16:59:30 -0300

    Update EnderecoResource

--- funcionario/src/main/java/br/com/funcionario/resource/v0/EnderecoResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/v0/EnderecoResource.java
@@ -1,6 +1,6 @@
 package br.com.funcionario.resource.v0;
 
-import br.com.funcionario.service.EnderecoService;
+import br.com.funcionario.service.v0.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
