0f81ce39bf30e5321be765abb119e86190184ca3
marciliodevjava <marciliodevjava@gmail.com>
2023-06-01 22:25:16 -0300

    Create EnderecoRetornoDto

---
+++ funcionario/src/main/java/br/com/funcionario/dto/response/EnderecoRetornoDto.java
@@ -0,0 +1,18 @@
+package br.com.funcionario.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class EnderecoRetornoDto {
+    private UUID identificadorEndereco;
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
