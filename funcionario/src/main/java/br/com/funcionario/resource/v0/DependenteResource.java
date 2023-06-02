8240f253d481fbae451d47d8ff7f6987d733d545
marciliodevjava <marciliodevjava@gmail.com>
2023-06-02 16:55:46 -0300

    Update DependenteResource

--- funcionario/src/main/java/br/com/funcionario/resource/v0/DependenteResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/v0/DependenteResource.java
@@ -1,7 +1,6 @@
 package br.com.funcionario.resource.v0;
 
-import br.com.funcionario.repository.DependenteRepositry;
-import br.com.funcionario.service.DependenteService;
+import br.com.funcionario.service.v0.DependenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
