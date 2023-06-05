fceeeebb8a156ce9ba844faa00e86e0d54b3a21a
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:29:53 -0300

    Create V8__insert-table-usuarios.sql

---
+++ gerador/src/main/resources/db/migration/V8__insert-table-usuarios.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuarios(login, senha)
+    value ("Zanatta", "$2a$10$B6CNpzXPz8sjDbSvxmxpcuLUXESfS/OVqxjaW/a0RqcnqzDQWa1wG"),
+    ("alessandraZat", "$2a$10$wqWaBpnXJVbv/FOGr7IpaOJkdf/DGuXYiQybtDIDA0KwRD3JNLiK6"),
+    ("alessandraZanatta", "$2a$10$A1DdFsCFsvaxHZ1RyxnJxeqLA/lVFGfYI116CZTW5QOzqB.N.obAS"),
+    ("alessandra", "$2a$10$Tsv4qXeeFNbGqPHbRtubHe1bQsSG0DIZ9keVgJ347VVmFq8rQCbZK"),
+    ("root", "$2a$10$B6CNpzXPz8sjDbSvxmxpcuLUXESfS/OVqxjaW/a0RqcnqzDQWa1wG"),
+    ("marcilio", "$2a$10$maWCm9wpChJLq97iojaYkOCr2YB9ruIqx.Ea6sp2B/j8vBthAoffW"),
+    ("marciliodev", "$2a$10$B6CNpzXPz8sjDbSvxmxpcuLUXESfS/OVqxjaW/a0RqcnqzDQWa1wG"),
+    ("marciliodev@teste.com.br", "$2a$10$H7mRygaf8.F2YmLNijTAXelWex5OFPbQ2PuQTMKbvRZLRGJot2mwG"),
+    ("marciliodev@teste.com", "5c12ea46b8c0320e07d8442f1d7d3f1f7fce6078dcfafa8bbebb9dd5f89ba5d6")
+    ;
