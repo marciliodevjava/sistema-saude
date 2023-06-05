9b6c2f750733d51d8935ce9244676b56201b2abf
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:49:52 -0300

    Update FuncionarioImp

--- funcionario/src/main/java/br/com/funcionario/resource/imp/v0/FuncionarioImp.java
+++ funcionario/src/main/java/br/com/funcionario/resource/imp/v0/FuncionarioImp.java
@@ -1,13 +1,16 @@
-package br.com.funcionario.resource.imp;
+package br.com.funcionario.resource.imp.v0;
 
 import br.com.funcionario.dto.FuncionarioDto;
+import br.com.funcionario.dto.response.FuncionarioRetornoDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.text.ParseException;
 import java.util.List;
 
 public interface FuncionarioImp {
 
-    public ResponseEntity<FuncionarioDto> salvaFuncionario(FuncionarioDto funcionarioDto);
+    public ResponseEntity<FuncionarioRetornoDto> salvaFuncionario(FuncionarioDto funcionarioDto, UriComponentsBuilder uriBuilder) throws ParseException;
     public ResponseEntity<FuncionarioDto> atualizarFuncionario(FuncionarioDto funcionarioDto);
     public ResponseEntity<List<FuncionarioDto>> listarFuncionario();
     public ResponseEntity<FuncionarioDto> listarFuncionarioId(Long id);
