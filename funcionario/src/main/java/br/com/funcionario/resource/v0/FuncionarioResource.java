662aa9daf20233863a6a41dd80513ab073cceda4
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:52:53 -0300

    Update FuncionarioResource

--- funcionario/src/main/java/br/com/funcionario/resource/v0/FuncionarioResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/v0/FuncionarioResource.java
@@ -1,25 +1,39 @@
 package br.com.funcionario.resource.v0;
 
 import br.com.funcionario.dto.FuncionarioDto;
-import br.com.funcionario.resource.imp.FuncionarioImp;
-import br.com.funcionario.service.FuncionarioService;
+import br.com.funcionario.dto.response.FuncionarioRetornoDto;
+import br.com.funcionario.resource.imp.v0.FuncionarioImp;
+import br.com.funcionario.service.v0.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioResource implements FuncionarioImp {
 
+    private URI uri;
     @Autowired
     private FuncionarioService funcionarioService;
 
+    @PostMapping("/salvar")
+    public ResponseEntity<FuncionarioRetornoDto> salvarSomenteFuncionario(FuncionarioDto funcionarioDto, UriComponentsBuilder uriBuilder){
+        FuncionarioRetornoDto funcionario = funcionarioService.salvarSomenteFuncionario(funcionarioDto);
+        this.uri = uriBuilder.path("/{id}").buildAndExpand(funcionario.getIdentificadorFuncionario()).toUri();
+        return ResponseEntity.created(uri).body(funcionario);
+    }
+
     @PostMapping
     @Override
-    public ResponseEntity<FuncionarioDto> salvaFuncionario(FuncionarioDto funcionarioDto) {
-        return null;
+    public ResponseEntity<FuncionarioRetornoDto> salvaFuncionario(FuncionarioDto funcionarioDto, UriComponentsBuilder uriBuilder) throws ParseException {
+        FuncionarioRetornoDto funcionario = funcionarioService.salvarFuncionario(funcionarioDto);
+        this.uri = uriBuilder.path("/{id}").buildAndExpand(funcionario.getIdentificadorFuncionario()).toUri();
+        return ResponseEntity.created(uri).body(funcionario);
     }
 
     @PutMapping
