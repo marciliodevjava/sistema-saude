503d82ac82f90e59ed9da51972e6a270c98b922b
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:50:16 -0300

    Update FuncionarioDto

--- funcionario/src/main/java/br/com/funcionario/dto/FuncionarioDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/FuncionarioDto.java
@@ -3,13 +3,17 @@ package br.com.funcionario.dto;
 import br.com.funcionario.domain.FuncionarioClt;
 import br.com.funcionario.domain.FuncionarioCnpj;
 import br.com.funcionario.domain.enuns.EstadoFuncionarioEnum;
+import br.com.funcionario.dto.request.FuncionarioCltDto;
+import br.com.funcionario.dto.request.FuncionarioCnpjDto;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FuncionarioDto {
+    private Integer numeroFuncionario;
+
     private EstadoFuncionarioEnum estadoFuncionarioEnum;
-    private FuncionarioClt funcionarioClt;
-    private FuncionarioCnpj funcionarioCnpj;
+    private FuncionarioCltDto funcionarioClt;
+    private FuncionarioCnpjDto funcionarioCnpj;
 }
