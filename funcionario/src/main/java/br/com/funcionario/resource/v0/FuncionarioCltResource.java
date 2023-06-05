07d028206fa82abc8ee132adcaa901391320a8aa
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:58:11 -0300

    Update FuncionarioCltResource

--- funcionario/src/main/java/br/com/funcionario/resource/v0/FuncionarioCltResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/v0/FuncionarioCltResource.java
@@ -1,6 +1,6 @@
 package br.com.funcionario.resource.v0;
 
-import br.com.funcionario.service.FuncionarioCltService;
+import br.com.funcionario.service.v0.FuncionarioCltService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
