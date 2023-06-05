c2ffed82bde4c7000b2f765679224901b6008fc8
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:52:30 -0300

    Update FuncionarioRetornoDto

--- funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioRetornoDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioRetornoDto.java
@@ -1,8 +1,6 @@
 package br.com.funcionario.dto.response;
 
 import br.com.funcionario.domain.enuns.EstadoFuncionarioEnum;
-import br.com.funcionario.dto.request.FuncionarioCltDto;
-import br.com.funcionario.dto.request.FuncionarioCnpjDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +9,7 @@ import java.util.UUID;
 @Getter
 @Setter
 public class FuncionarioRetornoDto {
-    private UUID identificadorFuncionario;
+    private String identificadorFuncionario;
     private Integer numeroFuncionario;
     private EstadoFuncionarioEnum estadoFuncionarioEnum;
     private FuncionarioCltRetornoDto funcionarioClt;
