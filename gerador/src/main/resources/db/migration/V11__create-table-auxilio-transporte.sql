51dff0cb2167bd270a2dc2c0db89ccaed768b5fd
marciliodevjava <marciliodevjava@gmail.com>
2023-06-05 09:27:26 -0300

    Create V11__create-table-auxilio-transporte.sql

---
+++ gerador/src/main/resources/db/migration/V11__create-table-auxilio-transporte.sql
@@ -0,0 +1,11 @@
+CREATE TABLE auxilio_transporte
+(
+    id                   bigint(10)     not null AUTO_INCREMENT,
+    identificador_auxilio_transporte varchar(36)    not null,
+    dias integer(10)    not null,
+    valor_passagem DECIMAL(10,2)    not null,
+    id_salario BIGINT(10),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_salario) references salarios(id)
+);
