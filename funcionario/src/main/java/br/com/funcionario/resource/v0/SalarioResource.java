c34e73655b495b5bbd33b08a104de0dc77fb304d
marciliodevjava <marciliodevjava@gmail.com>
2023-06-07 19:55:42 -0300

    Create SalarioResource

--- funcionario/src/main/java/br/com/funcionario/resource/v0/SalarioResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/v0/SalarioResource.java
@@ -1,7 +1,12 @@
 package br.com.funcionario.resource.v0;
 
+import br.com.funcionario.dto.request.SalarioDto;
+import br.com.funcionario.dto.response.SalarioRetornoDto;
 import br.com.funcionario.service.v0.SalarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,13 @@ public class SalarioResource {
 
     @Autowired
     private SalarioService salarioService;
+
+    @PostMapping("/gravar")
+    public ResponseEntity<SalarioRetornoDto> salvarSalario(@RequestBody SalarioDto salarioDto){
+
+        SalarioRetornoDto salarioRetornoDto = salarioService.savarSalario(salarioDto);
+
+        return ResponseEntity.ok(salarioRetornoDto);
+    }
 
 }
