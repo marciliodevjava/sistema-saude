a2ce4a74e60f39318b9364a9fc96f3209fb52f09
marciliodevjava <marciliodevjava@gmail.com>
2023-06-07 19:57:15 -0300

    Update Salario

--- funcionario/src/main/java/br/com/funcionario/domain/Salario.java
+++ funcionario/src/main/java/br/com/funcionario/domain/Salario.java
@@ -1,6 +1,7 @@
 package br.com.funcionario.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,14 +27,18 @@ public class Salario implements Serializable {
     private Long id;
     @Column(name = "identificador_salario", length = 36)
     private String identificadorSalario;
+    @Column(name = "salario")
     private BigDecimal salario;
     @Column(name = "valor_alimentacao")
     private BigDecimal valorAlimentacao;
+    @Column(name = "transporte")
     private BigDecimal transporte;
     @Getter(onMethod = @__({@JsonIgnore}))
+    @Setter(onMethod = @__({@JsonProperty}))
     @OneToOne(cascade = CascadeType.PERSIST, mappedBy = "salario", fetch = FetchType.LAZY)
     private AuxilioAlimentacao auxilioAlimentacao;
     @Getter(onMethod = @__({@JsonIgnore}))
+    @Setter(onMethod = @__({@JsonProperty}))
     @OneToOne(cascade = CascadeType.PERSIST, mappedBy = "salario", fetch = FetchType.LAZY)
     private AuxilioTransporte auxilioTransporte;
     @OneToOne
