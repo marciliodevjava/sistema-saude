233750e48d3eafe9747c15374ee74ffd3c4c3f52
marciliodevjava <marciliodevjava@gmail.com>
2023-06-07 19:57:28 -0300

    Create SalarioService

--- funcionario/src/main/java/br/com/funcionario/service/v0/SalarioService.java
+++ funcionario/src/main/java/br/com/funcionario/service/v0/SalarioService.java
@@ -1,7 +1,28 @@
 package br.com.funcionario.service.v0;
 
+import br.com.funcionario.domain.Salario;
+import br.com.funcionario.dto.request.SalarioDto;
+import br.com.funcionario.dto.response.SalarioRetornoDto;
+import br.com.funcionario.mapper.v0.SalarioMapper;
+import br.com.funcionario.repository.SalarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SalarioService {
+
+    @Autowired
+    private SalarioRepository salarioRepository;
+    @Autowired
+    private SalarioMapper salarioMapper;
+
+    public SalarioRetornoDto savarSalario(SalarioDto salarioDto) {
+
+        Salario salario = salarioMapper.MapearSalario(salarioDto);
+        salario= salarioRepository.save(salario);
+
+        SalarioRetornoDto retorno = salarioMapper.mapeiaRetornoSalario(salario);
+
+        return retorno;
+    }
 }
