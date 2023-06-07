ac236da150d09fc194e00fe0838f8ed7db8df192
marciliodevjava <marciliodevjava@gmail.com>
2023-06-07 19:45:58 -0300

    Create FeignClientConfig

---
+++ funcionario/src/main/java/br/com/funcionario/http/FeignClientConfig.java
@@ -0,0 +1,27 @@
+package br.com.funcionario.http;
+
+import br.com.funcionario.infra.security.TokenService;
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+public class FeignClientConfig {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String token = tokenService.getSubject(authentication.getName());
+
+            requestTemplate.header("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBUEkgR292ZXJuYW5jaWEiLCJzdWIiOiJaYW5hdHRhIiwiZXhwIjoxNjg2MTcxMTE4fQ.VZuIBcDzWLWykEdYSmIVcbWFGWSiOMQpRDmxiZ3gVxE");
+        };
+    }
+
+}
