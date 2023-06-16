5093290638284b51882f430bc6c7279b99c6b75b
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 12:37:18 -0300

    Create UuidRepository

---
+++ gerador/src/main/java/br/com/gerador/repository/UuidRepository.java
@@ -0,0 +1,9 @@
+package br.com.gerador.repository;
+
+import br.com.gerador.domain.Uuid;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UuidRepository extends JpaRepository<Uuid, Long> {
+}
