4366407b306ff20f2a5c9923460f5deb4f7df495
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 14:31:55 -0300

    Update V3__create-table-usuarios.sql

--- gerador/src/main/resources/db/migration/V3__create-table-usuarios.sql
+++ gerador/src/main/resources/db/migration/V3__create-table-usuarios.sql
@@ -2,5 +2,7 @@ CREATE TABLE usuarios(
     id    bigint not null auto_increment,
     login varchar(100) not null,
     senha varchar(100) not null,
+    data DATE,
+    hora TIME,
     primary key (id)
 );
