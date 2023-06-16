c8bb42ac8baf89f4954086687f8eee5db78789e7
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 14:28:35 -0300

    Update UuidRepository

--- gerador/src/main/java/br/com/gerador/repository/UuidRepository.java
+++ gerador/src/main/java/br/com/gerador/repository/UuidRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UuidRepository extends JpaRepository<Uuid, Long> {
+    Uuid findByuuidGerado(String uuidGerado);
 }
