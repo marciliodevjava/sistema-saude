484e3bce6da06ed5976fa7206ac7d77c52737246
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 14:28:18 -0300

    Update NumeroDto

--- gerador/src/main/java/br/com/gerador/dto/NumeroDto.java
+++ gerador/src/main/java/br/com/gerador/dto/NumeroDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class NumeroDto {
 
     private Integer numero;
+    private String identificadorNumero;
     private LocalDateTime localDateTime;
     private Long idFuncionario;
 }
