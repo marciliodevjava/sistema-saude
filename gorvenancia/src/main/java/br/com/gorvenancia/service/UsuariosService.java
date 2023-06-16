9a9b8a369e400cfbf13795781f85e6034a4ec3e3
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 14:17:48 -0300

    Update UsuarioService

--- gorvenancia/src/main/java/br/com/gorvenancia/service/UsuariosService.java
+++ gorvenancia/src/main/java/br/com/gorvenancia/service/UsuariosService.java
@@ -3,15 +3,25 @@ package br.com.gorvenancia.service;
 import br.com.gorvenancia.domain.Usuario;
 import br.com.gorvenancia.dto.UsuarioDto;
 import br.com.gorvenancia.repository.UsuarioRepository;
+import br.com.gorvenancia.util.FormatadorData;
+import br.com.gorvenancia.util.FormatadorHora;
 import br.com.gorvenancia.util.HashUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 @Service
 public class UsuariosService {
+    private LocalTime hora;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private FormatadorData formatadorData;
+    @Autowired
+    private FormatadorHora formatadorHora;
 
     public UsuarioDto createUsuario(UsuarioDto usuarioDto) {
         Usuario usuario = this.montarUsuario(usuarioDto);
@@ -27,7 +37,13 @@ public class UsuariosService {
     }
 
     private Usuario montarUsuario(UsuarioDto usuarioDto) {
+        Usuario usuario = new Usuario();
         String senha = HashUtil.getSecureHash(usuarioDto.getSenha());
-        return new Usuario(null, usuarioDto.getLogin(), senha);
+
+        usuario.setLogin(usuario.getLogin());
+        usuario.setSenha(senha);
+        usuario.setData(new Date());
+        usuario.setHora(hora);
+        return usuario;
     }
 }
