282792f40590d5889e9aceb1434e81bd9f3fa736
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 13:28:48 -0300

    Update GeradorUUID

--- gerador/src/main/java/br/com/gerador/utils/GeradorUUID.java
+++ gerador/src/main/java/br/com/gerador/utils/GeradorUUID.java
@@ -1,19 +1,27 @@
 package br.com.gerador.utils;
 
+import br.com.gerador.domain.Uuid;
+import br.com.gerador.repository.UuidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
 public class GeradorUUID {
+    @Autowired
+    private UuidRepository uuidRepository;
 
-    private UUID identificador;
+    public String getIdentificador(){
+        UUID identificador = UUID.randomUUID();
+        Uuid uuid;
+        String uuidGerado;
+        do {
+            uuidGerado = String.valueOf(identificador);
+            uuid = uuidRepository.findByuuidGerado(uuidGerado);
+        } while (uuid.equals(null));
 
-    public UUID getIdentificador(){
-//        do {
-            this.identificador = UUID.randomUUID();
-
-//        } while ();
-        return identificador;
+        uuid = uuidRepository.save(uuid);
+        return uuidGerado;
     }
 }
