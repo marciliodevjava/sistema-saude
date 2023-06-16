441ecd7188e6ea60d3016a4bbc296d5b237ed67d
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 12:36:12 -0300

    Create UUid

---
+++ gerador/src/main/java/br/com/gerador/domain/Uuid.java
@@ -0,0 +1,32 @@
+package br.com.gerador.domain;
+
+import br.com.gerador.domain.enuns.Projeto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "uuid")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Uuid {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "uuid_gerado")
+    private String uuidGerado;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "projeto", length = 20)
+    private Projeto projeto;
+    @Column(name = "data")
+    private Date data;
+    @Column(name = "hora")
+    private LocalTime hora;
+}
