93d6c5a8cb9c6068f838411eb73fc1af9d023bec
marciliodevjava <marciliodevjava@gmail.com>
2023-06-16 12:39:27 -0300

    Create V1__create-table-numeros.sql

--- gerador/src/main/resources/db/migration/V1__create-table-numeros.sql
+++ gerador/src/main/resources/db/migration/V1__create-table-numeros.sql
@@ -1,6 +1,10 @@
 CREATE TABLE numeros(
     id bigint not null AUTO_INCREMENT,
-    numero bigint not null,
+    id_funcionario bigint,
+    identificador_numero bigint,
+    matricula bigint not null,
     data DATETIME not null,
+    ativo bit,
+
     PRIMARY KEY (id)
 );
