d214b32fb47ebb4cdd1dacefda013b297ecce84e
marciliodevjava <marciliodevjava@gmail.com>
2023-06-19 08:30:25 -0300

    Update V2__create-table-uuid.sql

--- gerador/src/main/resources/db/migration/V2__create-table-uuid.sql
+++ gerador/src/main/resources/db/migration/V2__create-table-uuid.sql
@@ -2,7 +2,7 @@ CREATE TABLE uuid(
                         id bigint not null AUTO_INCREMENT,
                         uuid_gerado varchar(36) not null ,
                         projeto varchar(20),
-                        data DATETIME not null,
+                        data DATE not null,
                         hora TIME not null,
 
                         PRIMARY KEY (id)
