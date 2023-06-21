4772a8209f4fd98db33ba1220c906f141f8a1f42
marciliodevjava <marciliodevjava@gmail.com>
2023-06-21 18:30:28 -0300

    Update GeradorUUID

--- funcionario/src/main/java/br/com/funcionario/utils/GeradorUUID.java
+++ funcionario/src/main/java/br/com/funcionario/utils/GeradorUUID.java
@@ -1,17 +1,36 @@
 package br.com.funcionario.utils;
 
+import br.com.funcionario.domain.Uuid;
+import br.com.funcionario.domain.enuns.Projeto;
+import br.com.funcionario.repository.UuidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.UUID;
 
 @Component
 public class GeradorUUID {
-    private UUID identificador;
+    @Autowired
+    private UuidRepository uuidRepository;
 
     public UUID getIdentificador() {
-//        do {
-            this.identificador = UUID.randomUUID();
-//        } while();
+        UUID identificador;
+        Uuid uuid;
+        String uuidGerado;
+        do {
+            identificador = UUID.randomUUID();
+            uuidGerado = String.valueOf(identificador);
+            uuid = uuidRepository.findByuuidGerado(uuidGerado);
+        } while (uuid != null);
+
+        Uuid gravar = new Uuid();
+        gravar.setProjeto(Projeto.FUNCIONARIO);
+        gravar.setData(new Date());
+        gravar.setHora(LocalTime.now());
+        gravar.setUuidGerado(uuidGerado);
+        uuidRepository.save(gravar);
         return identificador;
     }
 }
