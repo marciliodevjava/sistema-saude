c5ac694550a9be5577bbf937e0740f7f1bb17243
marciliodevjava <marciliodevjava@gmail.com>
2023-06-22 15:11:55 -0300

    Update SalarioRepository

--- funcionario/src/main/java/br/com/funcionario/repository/SalarioRepository.java
+++ funcionario/src/main/java/br/com/funcionario/repository/SalarioRepository.java
@@ -4,6 +4,11 @@ import br.com.funcionario.domain.Salario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SalarioRepository extends JpaRepository<Salario, Long> {
+    Optional<Salario> findByFuncionarioClt(Long funcionarioClt);
+
+    Optional<Salario> findByFuncionarioCnpj(Long funcionarioCnpj);
 }
