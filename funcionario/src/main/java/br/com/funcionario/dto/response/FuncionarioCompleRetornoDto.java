3bbebfa06c82b16a023ed1d7be61ec190a7b3816
marciliodevjava <marciliodevjava@gmail.com>
2023-06-22 19:43:03 -0300

    Update FuncionarioCompleRetornoDto

--- funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioCompleRetornoDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioCompleRetornoDto.java
@@ -1,4 +1,15 @@
 package br.com.funcionario.dto.response;
 
+import br.com.funcionario.domain.enuns.EstadoFuncionarioEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FuncionarioCompleRetornoDto {
+    private String identificadorFunciona;
+    private Integer matricula;
+    private EstadoFuncionarioEnum situacao;
+    private FuncionarioCltRetornoDto funcionarioClt;
+    private FuncionarioCnpjRetornoDto funcionarioCnpj;
 }
