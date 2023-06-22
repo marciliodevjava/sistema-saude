bfce79678c0eecec16e7f33a41767d08a6ae1f77
marciliodevjava <marciliodevjava@gmail.com>
2023-06-22 16:17:32 -0300

    Update LeituraService

--- funcionario/src/main/java/br/com/funcionario/service/LeituraService.java
+++ funcionario/src/main/java/br/com/funcionario/service/LeituraService.java
@@ -1,11 +1,9 @@
 package br.com.funcionario.service;
 
 import br.com.funcionario.domain.*;
-import br.com.funcionario.dto.response.AuxilioAlimentacaoRetornoDto;
-import br.com.funcionario.dto.response.FuncionarioCltRetornoDto;
-import br.com.funcionario.dto.response.FuncionarioCnpjRetornoDto;
-import br.com.funcionario.dto.response.FuncionarioCompleRetornoDto;
+import br.com.funcionario.dto.response.*;
 import br.com.funcionario.repository.*;
+import br.com.funcionario.utils.ConverteDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +30,8 @@ public class LeituraService {
     private AuxilioAlimentacaoRepository auxilioAlimentacaoRepository;
     @Autowired
     private AuxilioTransporteRepository auxilioTransporteRepository;
+    @Autowired
+    private ConverteDate converteDate;
 
     public FuncionarioCompleRetornoDto funcionarioCompleto(Long id) {
         FuncionarioCompleRetornoDto funcionarioCompleRetornoDto = new FuncionarioCompleRetornoDto();
@@ -40,12 +40,17 @@ public class LeituraService {
         Optional<FuncionarioClt> funcionarioClt = funcionarioCltRepository.findByFuncionario((Long) funcionario.get().getId());
         Optional<FuncionarioCnpj> funcionarioCnpj = funcionarioCnpjRepository.findByFuncionario((Long) funcionario.get().getId());
 
-        if(Objects.nonNull(funcionarioClt)){
+        if (Objects.nonNull(funcionarioClt)) {
             List<Endereco> enderecosClt = enderecoRepository.findByFuncionarioClt((Long) funcionarioClt.get().getId());
+            funcionarioClt.get().setEndereco(enderecosClt);
             List<Dependente> dependentesClt = dependenteRepositry.findByFuncinarioClt((Long) funcionarioClt.get().getId());
+            funcionarioClt.get().setDependentesList(dependentesClt);
             Optional<Salario> salarioClt = salarioRepository.findByFuncionarioClt((Long) funcionarioClt.get().getId());
             Optional<AuxilioAlimentacao> auxilioAlimentacaoClt = auxilioAlimentacaoRepository.findBySalario((Long) salarioClt.get().getId());
+            salarioClt.get().setAuxilioAlimentacao(this.mapearAuxilioAlimentacao(auxilioAlimentacaoClt));
             Optional<AuxilioTransporte> auxilioTransporteClt = auxilioTransporteRepository.findBySalario((Long) salarioClt.get().getId());
+            salarioClt.get().setAuxilioTransporte(this.mapeiaAuxilioTransporte(auxilioTransporteClt));
+            funcionarioClt.get().setSalario(salarioClt.get());
         }
 
         if (Objects.nonNull(funcionarioCnpj)) {
@@ -59,24 +64,30 @@ public class LeituraService {
 
         funcionarioCompleRetornoDto = this.mapearFuncionario(funcionario);
 
-        if(Objects.nonNull(funcionarioClt)){
+        if (Objects.nonNull(funcionarioClt)) {
             FuncionarioCltRetornoDto funcionarioCltRetornoDto = new FuncionarioCltRetornoDto();
             funcionarioCltRetornoDto = this.mapearFuncionarioClt(funcionarioClt);
             funcionarioCompleRetornoDto.setFuncionarioClt(funcionarioCltRetornoDto);
         }
 
-        if(Objects.nonNull(funcionarioClt)){
+        if (Objects.nonNull(funcionarioCnpj)) {
             FuncionarioCnpjRetornoDto funcionarioCnpjRetornoDto = new FuncionarioCnpjRetornoDto();
-            funcionarioCompleRetornoDto.setFuncionarioCnpj(this.mapearFuncionarioCnpj(funcionarioCnpjRetornoDto));
+            funcionarioCompleRetornoDto.setFuncionarioCnpj(this.mapearFuncionarioCnpj(funcionarioCnpj));
             funcionarioCompleRetornoDto.setFuncionarioCnpj(funcionarioCnpjRetornoDto);
         }
 
         return funcionarioCompleRetornoDto;
     }
 
-    private FuncionarioCnpjRetornoDto mapearFuncionarioCnpj(FuncionarioCnpjRetornoDto funcionarioCnpjRetornoDto) {
+    private AuxilioTransporte mapeiaAuxilioTransporte(Optional<AuxilioTransporte> auxilioTransporteClt) {
+    }
 
-        FuncionarioCltRetornoDto funcionarioCltRetornoDto = new FuncionarioCltRetornoDto();
+    private AuxilioAlimentacao mapearAuxilioAlimentacao(Optional<AuxilioAlimentacao> auxilioAlimentacaoClt) {
+    }
+
+    private FuncionarioCnpjRetornoDto mapearFuncionarioCnpj(Optional<FuncionarioCnpj> funcionarioCnpj) {
+
+        FuncionarioCnpjRetornoDto funcionarioCnpjRetornoDto = new FuncionarioCnpjRetornoDto();
 
         return funcionarioCnpjRetornoDto;
     }
@@ -85,17 +96,35 @@ public class LeituraService {
 
         FuncionarioCltRetornoDto funcionarioCltRetornoDto = new FuncionarioCltRetornoDto();
 
-        if(Objects.nonNull(funcionarioClt)){
+        if (Objects.nonNull(funcionarioClt)) {
 
             funcionarioCltRetornoDto.setId(funcionarioClt.get().getId());
+            funcionarioCltRetornoDto.setIdentificadorFuncionarioClt(funcionarioClt.get().getIdentificadorFuncionarioClt());
+            funcionarioCltRetornoDto.setFuncaoFuncionarioEnum(funcionarioClt.get().getFuncaoFuncionarioEnum());
+            funcionarioCltRetornoDto.setEstadoCivil(funcionarioClt.get().getEstadoCivil());
+            funcionarioCltRetornoDto.setNome(funcionarioClt.get().getNome());
+            funcionarioCltRetornoDto.setDataNascimento(converteDate.converterDateParaString(funcionarioClt.get().getDataNascimento()));
+            funcionarioCltRetornoDto.setCpf(funcionarioClt.get().getCpf());
+            funcionarioCltRetornoDto.setRg(funcionarioClt.get().getRg());
+            funcionarioCltRetornoDto.setDdd(funcionarioClt.get().getDdd());
+            funcionarioCltRetornoDto.setTelefone(funcionarioClt.get().getTelefone());
+            funcionarioCltRetornoDto.setEndereco(this.mapearEnderecoClt(funcionarioClt.get().getEndereco()));
+            funcionarioCltRetornoDto.setDependentesList(this.mapearDependenteClt(funcionarioClt.get().getDependentesList()));
+
         }
 
         return funcionarioCltRetornoDto;
     }
 
+    private List<DependenteRetornoDto> mapearDependenteClt(List<Dependente> dependentesList) {
+    }
+
+    private List<EnderecoRetornoDto> mapearEnderecoClt(List<Endereco> endereco) {
+    }
+
     private FuncionarioCompleRetornoDto mapearFuncionario(Optional<Funcionario> funcionario) {
         FuncionarioCompleRetornoDto funcionarioCompleRetornoDto = new FuncionarioCompleRetornoDto();
-        if(Objects.nonNull(funcionario)){
+        if (Objects.nonNull(funcionario)) {
 
             funcionarioCompleRetornoDto.setId(funcionario.get().getId());
             funcionarioCompleRetornoDto.setIdentificadorFunciona(funcionario.get().getIdentificadorFuncionario());
