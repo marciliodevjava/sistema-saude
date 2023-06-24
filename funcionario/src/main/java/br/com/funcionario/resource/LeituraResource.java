8ba21ac937e1697dc222486a71052348db715533
marciliodevjava <marciliodevjava@gmail.com>
2023-06-23 22:58:33 -0300

    Update LeituraResource

--- funcionario/src/main/java/br/com/funcionario/resource/LeituraResource.java
+++ funcionario/src/main/java/br/com/funcionario/resource/LeituraResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/funcionario")
+@RequestMapping("/leitura")
 public class LeituraResource {
 
     @Autowired
