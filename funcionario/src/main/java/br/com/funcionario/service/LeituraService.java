a826519c5b3054324ec1d3fb4bfdd1613089057f
marciliodevjava <marciliodevjava@gmail.com>
2023-06-23 21:37:54 -0300

    Update LeituraService

--- funcionario/src/main/java/br/com/funcionario/service/LeituraService.java
+++ funcionario/src/main/java/br/com/funcionario/service/LeituraService.java
@@ -82,6 +82,187 @@ public class LeituraService {
         return funcionarioCompleRetornoDto;
     }
 
+    private FuncionarioCnpjRetornoDto mapearFuncionarioCnpj(Optional<FuncionarioCnpj> funcionarioCnpj1) {
+
+        FuncionarioCnpjRetornoDto funcionarioCnpjRetornoDto = new FuncionarioCnpjRetornoDto();
+
+        if(Objects.nonNull(funcionarioCnpj1)){
+
+            FuncionarioCnpj funcionarioCnpj = funcionarioCnpj1.get();
+
+            funcionarioCnpjRetornoDto.setIdentificadorFuncionarioCnpj(funcionarioCnpj.getIdentificadorFuncionarioCnpj());
+            funcionarioCnpjRetornoDto.setFuncaoFuncionarioEnum(funcionarioCnpj.getFuncaoFuncionarioEnum());
+            funcionarioCnpjRetornoDto.setEstadoCivil(funcionarioCnpj.getEstadoCivil());
+            funcionarioCnpjRetornoDto.setSalario(this.montarSalario(funcionarioCnpj.getSalario()));
+            funcionarioCnpjRetornoDto.setNome(funcionarioCnpj.getNome());
+            funcionarioCnpjRetornoDto.setDataAdmissao(funcionarioCnpj.getDataAdmissao());
+            funcionarioCnpjRetornoDto.setCpf(funcionarioCnpj.getCpf());
+            funcionarioCnpjRetornoDto.setCnpj(funcionarioCnpj.getCnpj());
+            funcionarioCnpjRetornoDto.setRg(funcionarioCnpj.getRg());
+            funcionarioCnpjRetornoDto.setDdd(funcionarioCnpj.getDdd());
+            funcionarioCnpjRetornoDto.setTelefone(funcionarioCnpj.getTelefone());
+            funcionarioCnpjRetornoDto.setEmail(funcionarioCnpj.getEmail());
+            funcionarioCnpjRetornoDto.setDataAdmissao(funcionarioCnpj.getDataAdmissao());
+            funcionarioCnpjRetornoDto.setHorarioInicial(funcionarioCnpj.getHoraInicial());
+            funcionarioCnpjRetornoDto.setHorarioFinal(funcionarioCnpj.getHoraFinal());
+            funcionarioCnpjRetornoDto.setAtivo(funcionarioCnpj.getAtivo());
+            funcionarioCnpjRetornoDto.setDependentes(this.mapearDependente(funcionarioCnpj.getDependentesList()));
+            funcionarioCnpjRetornoDto.setEndereco(this.mapearEndereco(funcionarioCnpj.getEndereco()));
+
+            return funcionarioCnpjRetornoDto;
+        }
+
+        return null;
+    }
+
+    private FuncionarioCltRetornoDto mapearFuncionarioClt(Optional<FuncionarioClt> funcionarioClt1) {
+
+        FuncionarioCltRetornoDto funcionarioCltRetornoDto = new FuncionarioCltRetornoDto();
+
+        if (Objects.nonNull(funcionarioClt1)) {
+
+            FuncionarioClt funcionarioClt = funcionarioClt1.get();
+
+            funcionarioCltRetornoDto.setIdentificadorFuncionarioClt(funcionarioClt.getIdentificadorFuncionarioClt());
+            funcionarioCltRetornoDto.setFuncaoFuncionarioEnum(funcionarioClt.getFuncaoFuncionarioEnum());
+            funcionarioCltRetornoDto.setEstadoCivil(funcionarioClt.getEstadoCivil());
+            funcionarioCltRetornoDto.setSalario(this.montarSalario(funcionarioClt.getSalario()));
+            funcionarioCltRetornoDto.setNome(funcionarioClt.getNome());
+            funcionarioCltRetornoDto.setDataNascimento(converteDate.converterDateParaString(funcionarioClt.getDataNascimento()));
+            funcionarioCltRetornoDto.setCpf(funcionarioClt.getCpf());
+            funcionarioCltRetornoDto.setRg(funcionarioClt.getRg());
+            funcionarioCltRetornoDto.setDdd(funcionarioClt.getDdd());
+            funcionarioCltRetornoDto.setTelefone(funcionarioClt.getTelefone());
+            funcionarioCltRetornoDto.setEmail(funcionarioClt.getEmail());
+            funcionarioCltRetornoDto.setDataAdmissao(funcionarioClt.getDataAdmissao());
+            funcionarioCltRetornoDto.setHoraInicial(funcionarioClt.getHoraInicial());
+            funcionarioCltRetornoDto.setHoraFinal(funcionarioClt.getHoraFinal());
+            funcionarioCltRetornoDto.setAtivo(funcionarioClt.getAtivo());
+            funcionarioCltRetornoDto.setEndereco(this.mapearEndereco(funcionarioClt.getEndereco()));
+            funcionarioCltRetornoDto.setDependentesList(this.mapearDependente(funcionarioClt.getDependentesList()));
+
+            return funcionarioCltRetornoDto;
+        }
+
+        return null;
+    }
+
+    private SalarioRetornoDto montarSalario(Salario salario) {
+
+        SalarioRetornoDto salarioRetornoDto = new SalarioRetornoDto();
+
+        if (Objects.nonNull(salario)) {
+
+            salarioRetornoDto.setIdentificadorSalario(salario.getIdentificadorSalario());
+            salarioRetornoDto.setSalario(salario.getSalario());
+            salarioRetornoDto.setValorAlimentacao(salario.getValorAlimentacao());
+            salarioRetornoDto.setTransporte(salario.getTransporte());
+            salarioRetornoDto.setAuxilioAlimentacao(this.mapearAuxilioAlimentacao(salario.getAuxilioAlimentacao()));
+            salarioRetornoDto.setAuxilioTransporte(this.mapeiaAuxilioTransporte(salario.getAuxilioTransporte()));
+
+            return salarioRetornoDto;
+        }
+
+        return null;
+    }
+
+    private AuxilioTransporteRetornoDto mapeiaAuxilioTransporte(AuxilioTransporte auxilioTransporte) {
+
+        AuxilioTransporteRetornoDto auxilioTransporteRetornoDto = new AuxilioTransporteRetornoDto();
+
+        if (Objects.nonNull(auxilioTransporte)){
+
+            auxilioTransporteRetornoDto.setIdentificadorAuxilioTransporte(auxilioTransporte.getIdentificadorAuxilioTransporte());
+            auxilioTransporteRetornoDto.setDias(auxilioTransporte.getDias());
+            auxilioTransporte.setValorPassagem(auxilioTransporte.getValorPassagem());
+
+            return auxilioTransporteRetornoDto;
+        }
+
+        return null;
+    }
+
+    private AuxilioAlimentacaoRetornoDto mapearAuxilioAlimentacao(AuxilioAlimentacao auxilioAlimentacao) {
+
+        AuxilioAlimentacaoRetornoDto auxilioAlimentacaoRetornoDto = new AuxilioAlimentacaoRetornoDto();
+
+        if (Objects.nonNull(auxilioAlimentacao)) {
+
+            auxilioAlimentacaoRetornoDto.setIdentificadorAuxilioTransporte(auxilioAlimentacao.getIdentificadorAuxilioAlimentacao());
+            auxilioAlimentacaoRetornoDto.setDias(auxilioAlimentacao.getDias());
+            auxilioAlimentacaoRetornoDto.setValor(auxilioAlimentacao.getValor());
+
+            return auxilioAlimentacaoRetornoDto;
+        }
+
+        return null;
+    }
+
+    private List<DependenteRetornoDto> mapearDependente(List<Dependente> dependentesList) {
+
+        List<DependenteRetornoDto> dependenteRetorno = new ArrayList<>();
+
+        if (Objects.nonNull(dependentesList)) {
+            dependentesList.forEach(e -> {
+
+                DependenteRetornoDto dependenteRetornoDto = new DependenteRetornoDto();
+
+                dependenteRetornoDto.setIdentificadorEndereco(e.getIdentificadorDependente());
+                dependenteRetornoDto.setNome(e.getNome());
+                dependenteRetornoDto.setCpf(e.getCpf());
+                dependenteRetornoDto.setRg(e.getRg());
+                dependenteRetornoDto.setDataNascimento(e.getDataNascimento());
+                dependenteRetornoDto.setGrauParentescoEnum(e.getGrauParentescoEnum());
+
+                dependenteRetorno.add(dependenteRetornoDto);
+            });
+
+            return dependenteRetorno;
+        }
+
+        return Collections.emptyList();
+    }
+
+    private List<EnderecoRetornoDto> mapearEndereco(List<Endereco> endereco) {
+
+        List<EnderecoRetornoDto> enderecoRetorno = new ArrayList<>();
+
+        if (Objects.nonNull(endereco)) {
+            endereco.forEach(e -> {
+
+                EnderecoRetornoDto enderecoRetornoDto = new EnderecoRetornoDto();
+
+                enderecoRetornoDto.setIdentificadorEndereco(e.getIdentificadorEndereco());
+                enderecoRetornoDto.setCep(e.getCep());
+                enderecoRetornoDto.setLogradouro(e.getLogradouro());
+                enderecoRetornoDto.setNumero(e.getNumero());
+                enderecoRetornoDto.setBairro(e.getBairro());
+                enderecoRetornoDto.setCidade(e.getCidade());
+                enderecoRetornoDto.setUf(e.getUf());
+
+                enderecoRetorno.add(enderecoRetornoDto);
+            });
+
+            return enderecoRetorno;
+        }
+
+        return Collections.emptyList();
+    }
+
+    private FuncionarioCompleRetornoDto mapearFuncionario(Optional<Funcionario> funcionario) {
+        FuncionarioCompleRetornoDto funcionarioCompleRetornoDto = new FuncionarioCompleRetornoDto();
+        if (Objects.nonNull(funcionario)) {
+
+            funcionarioCompleRetornoDto.setIdentificadorFunciona(funcionario.get().getIdentificadorFuncionario());
+            funcionarioCompleRetornoDto.setMatricula(funcionario.get().getNumeroFuncionario());
+
+            return funcionarioCompleRetornoDto;
+        }
+
+        return null;
+    }
+
+
     private AuxilioTransporte mapeiaAuxilioTransporte(Optional<AuxilioTransporte> auxilioTransporteClt) {
 
         AuxilioTransporte auxilioTransporte = new AuxilioTransporte();
@@ -114,165 +295,5 @@ public class LeituraService {
         }
 
         return null;
-    }
-
-    private FuncionarioCnpjRetornoDto mapearFuncionarioCnpj(Optional<FuncionarioCnpj> funcionarioCnpj) {
-
-        FuncionarioCnpjRetornoDto funcionarioCnpjRetornoDto = new FuncionarioCnpjRetornoDto();
-
-        if(Objects.nonNull(funcionarioCnpj)){
-
-            funcionarioCnpjRetornoDto.setIdentificadorFuncionarioCnpj(funcionarioCnpj.get().getIdentificadorFuncionarioCnpj());
-            funcionarioCnpjRetornoDto.setFuncaoFuncionarioEnum(funcionarioCnpj.get().getFuncaoFuncionarioEnum());
-            funcionarioCnpjRetornoDto.setEstadoCivil(funcionarioCnpj.get().getEstadoCivil());
-            funcionarioCnpjRetornoDto.setSalario(this.montarSalario(funcionarioCnpj.get().getSalario()));
-            funcionarioCnpjRetornoDto.setNome(funcionarioCnpj.get().getNome());
-
-
-            return funcionarioCnpjRetornoDto;
-        }
-
-        return null;
-    }
-
-    private FuncionarioCltRetornoDto mapearFuncionarioClt(Optional<FuncionarioClt> funcionarioClt) {
-
-        FuncionarioCltRetornoDto funcionarioCltRetornoDto = new FuncionarioCltRetornoDto();
-
-        if (Objects.nonNull(funcionarioClt)) {
-
-            funcionarioCltRetornoDto.setIdentificadorFuncionarioClt(funcionarioClt.get().getIdentificadorFuncionarioClt());
-            funcionarioCltRetornoDto.setFuncaoFuncionarioEnum(funcionarioClt.get().getFuncaoFuncionarioEnum());
-            funcionarioCltRetornoDto.setEstadoCivil(funcionarioClt.get().getEstadoCivil());
-            funcionarioCltRetornoDto.setSalario(this.montarSalario(funcionarioClt.get().getSalario()));
-            funcionarioCltRetornoDto.setNome(funcionarioClt.get().getNome());
-            funcionarioCltRetornoDto.setDataNascimento(converteDate.converterDateParaString(funcionarioClt.get().getDataNascimento()));
-            funcionarioCltRetornoDto.setCpf(funcionarioClt.get().getCpf());
-            funcionarioCltRetornoDto.setRg(funcionarioClt.get().getRg());
-            funcionarioCltRetornoDto.setDdd(funcionarioClt.get().getDdd());
-            funcionarioCltRetornoDto.setTelefone(funcionarioClt.get().getTelefone());
-            funcionarioCltRetornoDto.setEmail(funcionarioClt.get().getEmail());
-            funcionarioCltRetornoDto.setDataAdmissao(funcionarioClt.get().getDataAdmissao());
-            funcionarioCltRetornoDto.setHoraInicial(funcionarioClt.get().getHoraInicial());
-            funcionarioCltRetornoDto.setHoraFinal(funcionarioClt.get().getHoraFinal());
-            funcionarioCltRetornoDto.setAtivo(funcionarioClt.get().getAtivo());
-            funcionarioCltRetornoDto.setEndereco(this.mapearEnderecoClt(funcionarioClt.get().getEndereco()));
-            funcionarioCltRetornoDto.setDependentesList(this.mapearDependenteClt(funcionarioClt.get().getDependentesList()));
-
-            return funcionarioCltRetornoDto;
-        }
-
-        return null;
-    }
-
-    private SalarioRetornoDto montarSalario(Salario salario) {
-
-        SalarioRetornoDto salarioRetornoDto = new SalarioRetornoDto();
-
-        if (Objects.nonNull(salario)) {
-
-            salarioRetornoDto.setIdentificadorSalario(salario.getIdentificadorSalario());
-            salarioRetornoDto.setSalario(salario.getSalario());
-            salarioRetornoDto.setValorAlimentacao(salario.getValorAlimentacao());
-            salarioRetornoDto.setTransporte(salario.getTransporte());
-            salarioRetornoDto.setAuxilioAlimentacao(this.mapearAuxilioAlimentacaoDto(salario.getAuxilioAlimentacao()));
-            salarioRetornoDto.setAuxilioTransporte(this.mapeiaAuxilioTransporteDto(salario.getAuxilioTransporte()));
-        }
-
-        return null;
-    }
-
-    private AuxilioTransporteRetornoDto mapeiaAuxilioTransporteDto(AuxilioTransporte auxilioTransporte) {
-
-        AuxilioTransporteRetornoDto auxilioTransporteRetornoDto = new AuxilioTransporteRetornoDto();
-
-        if (Objects.nonNull(auxilioTransporte)){
-
-            auxilioTransporteRetornoDto.setIdentificadorAuxilioTransporte(auxilioTransporte.getIdentificadorAuxilioTransporte());
-            auxilioTransporteRetornoDto.setDias(auxilioTransporte.getDias());
-            auxilioTransporte.setValorPassagem(auxilioTransporte.getValorPassagem());
-
-            return auxilioTransporteRetornoDto;
-        }
-
-        return null;
-    }
-
-    private AuxilioAlimentacaoRetornoDto mapearAuxilioAlimentacaoDto(AuxilioAlimentacao auxilioAlimentacao) {
-
-        AuxilioAlimentacaoRetornoDto auxilioAlimentacaoRetornoDto = new AuxilioAlimentacaoRetornoDto();
-
-        if (Objects.nonNull(auxilioAlimentacao)) {
-
-            auxilioAlimentacaoRetornoDto.setIdentificadorAuxilioTransporte(auxilioAlimentacao.getIdentificadorAuxilioAlimentacao());
-            auxilioAlimentacaoRetornoDto.setDias(auxilioAlimentacao.getDias());
-            auxilioAlimentacaoRetornoDto.setValor(auxilioAlimentacao.getValor());
-
-            return auxilioAlimentacaoRetornoDto;
-        }
-
-        return null;
-    }
-
-    private List<DependenteRetornoDto> mapearDependenteClt(List<Dependente> dependentesList) {
-
-        List<DependenteRetornoDto> dependenteRetorno = new ArrayList<>();
-
-        if (Objects.nonNull(dependentesList)) {
-            dependentesList.forEach(e -> {
-
-                DependenteRetornoDto dependenteRetornoDto = new DependenteRetornoDto();
-
-                dependenteRetornoDto.setIdentificadorEndereco(e.getIdentificadorDependente());
-                dependenteRetornoDto.setNome(e.getNome());
-                dependenteRetornoDto.setCpf(e.getCpf());
-                dependenteRetornoDto.setRg(e.getRg());
-                dependenteRetornoDto.setDataNascimento(e.getDataNascimento());
-                dependenteRetornoDto.setGrauParentescoEnum(e.getGrauParentescoEnum());
-
-                dependenteRetorno.add(dependenteRetornoDto);
-            });
-
-            return dependenteRetorno;
-        }
-
-        return Collections.emptyList();
-    }
-
-    private List<EnderecoRetornoDto> mapearEnderecoClt(List<Endereco> endereco) {
-
-        List<EnderecoRetornoDto> enderecoRetorno = new ArrayList<>();
-
-        if (Objects.nonNull(endereco)) {
-            endereco.forEach(e -> {
-
-                EnderecoRetornoDto enderecoRetornoDto = new EnderecoRetornoDto();
-
-                enderecoRetornoDto.setIdentificadorEndereco(e.getIdentificadorEndereco());
-                enderecoRetornoDto.setCep(e.getCep());
-                enderecoRetornoDto.setLogradouro(e.getLogradouro());
-                enderecoRetornoDto.setNumero(e.getNumero());
-                enderecoRetornoDto.setBairro(e.getBairro());
-                enderecoRetornoDto.setCidade(e.getCidade());
-                enderecoRetornoDto.setUf(e.getUf());
-
-                enderecoRetorno.add(enderecoRetornoDto);
-            });
-
-            return enderecoRetorno;
-        }
-
-        return Collections.emptyList();
-    }
-
-    private FuncionarioCompleRetornoDto mapearFuncionario(Optional<Funcionario> funcionario) {
-        FuncionarioCompleRetornoDto funcionarioCompleRetornoDto = new FuncionarioCompleRetornoDto();
-        if (Objects.nonNull(funcionario)) {
-
-            funcionarioCompleRetornoDto.setIdentificadorFunciona(funcionario.get().getIdentificadorFuncionario());
-            funcionarioCompleRetornoDto.setMatricula(funcionario.get().getNumeroFuncionario());
-        }
-
-        return funcionarioCompleRetornoDto;
     }
 }
