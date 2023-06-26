07f659b87e3332005e5a7174d23910db046c2fff
marciliodevjava <marciliodevjava@gmail.com>
2023-06-26 15:46:44 -0300

    Update AuxilioTransporteRepository

--- funcionario/src/main/java/br/com/funcionario/repository/AuxilioTransporteRepository.java
+++ funcionario/src/main/java/br/com/funcionario/repository/AuxilioTransporteRepository.java
@@ -1,6 +1,7 @@
 package br.com.funcionario.repository;
 
 import br.com.funcionario.domain.AuxilioTransporte;
+import br.com.funcionario.domain.Salario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface AuxilioTransporteRepository extends JpaRepository<AuxilioTransporte, Long> {
-    Optional<AuxilioTransporte> findBySalario(Long salario);
+    Optional<AuxilioTransporte> findBySalario(Salario salario);
 }
