ea351eca31ffd5f1bca424caba3565354b8a0a53
marciliodevjava <marciliodevjava@gmail.com>
2023-06-26 15:46:25 -0300

    Update DependenteRepositry

--- funcionario/src/main/java/br/com/funcionario/repository/DependenteRepositry.java
+++ funcionario/src/main/java/br/com/funcionario/repository/DependenteRepositry.java
@@ -1,6 +1,8 @@
 package br.com.funcionario.repository;
 
 import br.com.funcionario.domain.Dependente;
+import br.com.funcionario.domain.FuncionarioClt;
+import br.com.funcionario.domain.FuncionarioCnpj;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface DependenteRepositry extends JpaRepository<Dependente, Long> {
-   List<Dependente> findByFuncionarioClt(Long funcionarioClt);
+   List<Dependente> findByFuncionarioClt(FuncionarioClt funcionarioClt);
 
-    List<Dependente> findByFuncionarioCnpj(Long funcionarioCnpj);
+    List<Dependente> findByFuncionarioCnpj(FuncionarioCnpj funcionarioCnpj);
 }
