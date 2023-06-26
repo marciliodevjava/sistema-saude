1f556d702557ce431d5117139020843d8de6929a
marciliodevjava <marciliodevjava@gmail.com>
2023-06-26 15:45:46 -0300

    Update FuncionarioCltRepository

--- funcionario/src/main/java/br/com/funcionario/repository/FuncionarioCltRepository.java
+++ funcionario/src/main/java/br/com/funcionario/repository/FuncionarioCltRepository.java
@@ -1,5 +1,6 @@
 package br.com.funcionario.repository;
 
+import br.com.funcionario.domain.Funcionario;
 import br.com.funcionario.domain.FuncionarioClt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface FuncionarioCltRepository extends JpaRepository<FuncionarioClt, Long> {
-    Optional<FuncionarioClt> findByFuncionario(Long funcionario);
+    Optional<FuncionarioClt> findByFuncionario(Funcionario funcionario);
 }
