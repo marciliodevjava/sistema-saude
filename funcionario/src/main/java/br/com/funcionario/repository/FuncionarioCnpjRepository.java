ce41b19be2278854fbeaa754f98db8a52d64923e
marciliodevjava <marciliodevjava@gmail.com>
2023-06-26 15:45:29 -0300

    Update FuncionarioCnpjRepository

--- funcionario/src/main/java/br/com/funcionario/repository/FuncionarioCnpjRepository.java
+++ funcionario/src/main/java/br/com/funcionario/repository/FuncionarioCnpjRepository.java
@@ -1,5 +1,6 @@
 package br.com.funcionario.repository;
 
+import br.com.funcionario.domain.Funcionario;
 import br.com.funcionario.domain.FuncionarioCnpj;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface FuncionarioCnpjRepository extends JpaRepository<FuncionarioCnpj, Long> {
-    Optional<FuncionarioCnpj> findByFuncionario(Long funcionario);
+    Optional<FuncionarioCnpj> findByFuncionario(Funcionario funcionario);
 }
