ce07ea612a3e91574000a0a9b86972f71dd37bf6
marciliodevjava <marciliodevjava@gmail.com>
2023-06-27 16:13:27 -0300

    Update FuncionarioRetornoDto

--- funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioRetornoDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioRetornoDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FuncionarioRetornoDto {
+    private Long id;
     private String identificadorFuncionario;
     private Integer numeroFuncionario;
     private EstadoFuncionarioEnum estadoFuncionarioEnum;
