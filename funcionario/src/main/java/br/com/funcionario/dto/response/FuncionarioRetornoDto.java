9e9a4e4b1223784bf47646119616e5eb00025858
marciliodevjava <marciliodevjava@gmail.com>
2023-06-27 12:01:30 -0300

    Update FuncionarioRetornoDto

--- funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioRetornoDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/response/FuncionarioRetornoDto.java
@@ -4,8 +4,6 @@ import br.com.funcionario.domain.enuns.EstadoFuncionarioEnum;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 public class FuncionarioRetornoDto {
