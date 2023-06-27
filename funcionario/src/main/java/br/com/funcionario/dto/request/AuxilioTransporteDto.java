e42f61abe510cedef2d0e5ce28c824d6c3a27714
marciliodevjava <marciliodevjava@gmail.com>
2023-06-27 10:48:33 -0300

    Update AuxilioTransporteDto

--- funcionario/src/main/java/br/com/funcionario/dto/request/AuxilioTransporteDto.java
+++ funcionario/src/main/java/br/com/funcionario/dto/request/AuxilioTransporteDto.java
@@ -3,12 +3,11 @@ package br.com.funcionario.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 
 @Getter
 @Setter
 public class AuxilioTransporteDto {
     private int dias;
-    private BigDecimal valorPassagem;
+    private Double valorPassagem;
 
 }
