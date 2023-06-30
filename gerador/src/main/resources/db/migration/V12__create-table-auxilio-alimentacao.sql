f38d44c6fbef78a36a2b8eb35b678b0df2712fd4
marciliodevjava <marciliodevjava@gmail.com>
2023-06-30 18:54:56 -0300

    Update V12__create-table-auxilio-alimentacao.sql

--- gerador/src/main/resources/db/migration/V12__create-table-auxilio-alimentacao.sql
+++ gerador/src/main/resources/db/migration/V12__create-table-auxilio-alimentacao.sql
@@ -1,11 +1,11 @@
 CREATE TABLE auxilio_alimentacao
 (
-    id                   bigint(10)     not null AUTO_INCREMENT,
+    id                                bigint(10)     not null AUTO_INCREMENT,
     identificador_auxilio_alimentacao varchar(36)    not null,
-    dias integer(10)    not null,
-    valor DECIMAL(10,2)    not null,
-    id_salario BIGINT(10),
+    dias                              integer(10)    not null,
+    valor                             DECIMAL(10, 2) not null,
+    id_salario                        BIGINT(10),
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_salario) references salarios(id)
+    FOREIGN KEY (id_salario) references salarios (id)
 );
