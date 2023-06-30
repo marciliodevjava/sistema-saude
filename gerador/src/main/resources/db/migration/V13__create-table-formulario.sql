cf30aa4531db6d9c1c7fdd6d537ffe66a423455a
marciliodevjava <marciliodevjava@gmail.com>
2023-06-30 18:55:11 -0300

    Update V13__create-table-formulario.sql

--- gerador/src/main/resources/db/migration/V13__create-table-formulario.sql
+++ gerador/src/main/resources/db/migration/V13__create-table-formulario.sql
@@ -1,8 +1,8 @@
 CREATE TABLE formulario_paciente
 (
-    id bigint(10) not null AUTO_INCREMENT,
-    id_formulario varchar(36)    not null,
-    data DATE not null,
-    hora TIME not null,
+    id            bigint(10)  not null AUTO_INCREMENT,
+    id_formulario varchar(36) not null,
+    data          DATE        not null,
+    hora          TIME        not null,
     PRIMARY KEY (id)
 );
