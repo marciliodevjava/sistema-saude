95ddc82c8f2416b9c360f236e77b32e65e300fb0
marciliodevjava <marciliodevjava@gmail.com>
2023-06-30 18:52:41 -0300

    Update V4__create-table-funcionario.sql

--- gerador/src/main/resources/db/migration/V4__create-table-funcionario.sql
+++ gerador/src/main/resources/db/migration/V4__create-table-funcionario.sql
@@ -1,11 +1,11 @@
 CREATE TABLE funcionarios
 (
-    id    bigint(16)      not null auto_increment,
-    identificador_funcionario varchar(36) not null,
-    numero_funcionario integer(20) not null,
-    estado_funcionario varchar(15) not null,
-    in_principal_funcionario_clt BOOLEAN not null ,
-    in_principal_funcionario_cnpj BOOLEAN not null,
+    id                            bigint(16)  not null auto_increment,
+    identificador_funcionario     varchar(36) not null,
+    numero_funcionario            integer(20) not null,
+    estado_funcionario            varchar(15) not null,
+    in_principal_funcionario_clt  BOOLEAN     not null,
+    in_principal_funcionario_cnpj BOOLEAN     not null,
 
     primary key (id)
 );
