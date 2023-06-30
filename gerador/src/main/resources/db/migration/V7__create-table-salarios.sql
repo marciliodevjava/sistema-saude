56769c353704441be0d5ce0cb6b794b49a334049
marciliodevjava <marciliodevjava@gmail.com>
2023-06-30 18:53:19 -0300

    Update V7__create-table-salarios.sql

--- gerador/src/main/resources/db/migration/V7__create-table-salarios.sql
+++ gerador/src/main/resources/db/migration/V7__create-table-salarios.sql
@@ -1,14 +1,14 @@
 CREATE TABLE salarios
 (
-    id                   bigint(10)     not null AUTO_INCREMENT,
+    id                    bigint(10)     not null AUTO_INCREMENT,
     identificador_salario varchar(36)    not null,
-    salario              DECIMAL(10, 2) not null,
-    valor_alimentacao DECIMAL(10, 2) not null,
-    transporte           DECIMAL(10, 2) not null,
-    id_funcionario_clt bigint(10),
-    id_funcionario_cnpj bigint(10),
+    salario               DECIMAL(10, 2) not null,
+    valor_alimentacao     DECIMAL(10, 2) not null,
+    transporte            DECIMAL(10, 2) not null,
+    id_funcionario_clt    bigint(10),
+    id_funcionario_cnpj   bigint(10),
 
     PRIMARY KEY (id),
-    FOREIGN KEY (id_funcionario_clt) references funcionarios_clt(id),
-    FOREIGN KEY (id_funcionario_cnpj) references funcionarios_cnpj(id)
+    FOREIGN KEY (id_funcionario_clt) references funcionarios_clt (id),
+    FOREIGN KEY (id_funcionario_cnpj) references funcionarios_cnpj (id)
 );
