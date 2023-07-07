f25c955dbbb285a61d5899762aca817681985c75
marciliodevjava <marciliodevjava@gmail.com>
2023-07-06 23:07:03 -0300

    Curso certificação OCA

---
+++ certificao-java-oca/src/main/java/org/example/certificaoJevaSE7Programmer/DenifirOEscopoVariavel.java
@@ -0,0 +1,15 @@
+package org.example.certificaoJevaSE7Programmer;
+
+public class DenifirOEscopoVariavel {
+
+    public static void main(String[] args) {
+        DenifirOEscopoVariavel dev = new DenifirOEscopoVariavel();
+        dev.m1();
+    }
+
+    private void m1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Número: " + i);
+        }
+    }
+}
