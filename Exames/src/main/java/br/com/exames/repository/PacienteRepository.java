3397c1bae51d6cba6a8a11d158fa30ba9c333073
marciliodevjava <marciliodevjava@gmail.com>
2023-07-24 21:35:53 -0300

    Update PacienteRepository

--- Exames/src/main/java/br/com/exames/repository/PacienteRepository.java
+++ Exames/src/main/java/br/com/exames/repository/PacienteRepository.java
@@ -5,7 +5,10 @@ import br.com.exames.domain.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-    Paciente findByFormulario(Long id);
+
+    Optional<Paciente> findByFormulario(Formulario formulario);
 }
