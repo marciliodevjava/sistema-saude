f166d7766c8c7349841fa419ebb6245ad27dfef2
marciliodevjava <marciliodevjava@gmail.com>
2023-07-24 21:36:04 -0300

    Update EnderecoRepository

--- Exames/src/main/java/br/com/exames/repository/EnderecoRepository.java
+++ Exames/src/main/java/br/com/exames/repository/EnderecoRepository.java
@@ -1,12 +1,17 @@
 package br.com.exames.repository;
 
 import br.com.exames.domain.Endereco;
+import br.com.exames.domain.Medico;
+import br.com.exames.domain.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-    Endereco findByMedico(Long id);
 
-    Endereco findByPaciente(Long id);
+    Optional<Endereco> findByMedico(Medico medico);
+
+    Optional<Endereco> findByPaciente(Paciente paciente);
 }
