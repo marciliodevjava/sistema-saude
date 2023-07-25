53fea325dc042abcbbb78c156f606c0939da6df0
marciliodevjava <marciliodevjava@gmail.com>
2023-07-24 21:35:39 -0300

    Update MedicoRepository

--- Exames/src/main/java/br/com/exames/repository/MedicoRepository.java
+++ Exames/src/main/java/br/com/exames/repository/MedicoRepository.java
@@ -5,8 +5,11 @@ import br.com.exames.domain.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
-    Medico findByFormulario(Long id);
+    Optional<Medico> findByFormulario(Formulario formulario);
+
 }
